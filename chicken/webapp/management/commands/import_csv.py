f258b33bcc56bad3a34455a7c692740eac3a3d53
Patrick Rodriguez <patr84@gmail.com>
2013-12-22 17:10:46 -0800

    Updated import_csv command to import all data with correct timezone

--- chicken/webapp/management/commands/import_csv.py
+++ chicken/webapp/management/commands/import_csv.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.core.management.base import BaseCommand
 from csv import reader
+from pytz import timezone
 
 from webapp.models import SensorData
 
@@ -12,11 +13,16 @@ class Command(BaseCommand):
         chicken_file = open('/Users/patr/PycharmProjects/chicken_project/chickens.txt')
         csv_reader = reader(chicken_file)
         for i, row in enumerate(csv_reader):
+            # parse row data
             timestamp = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+            timestamp = timestamp.replace(tzinfo=timezone('US/Pacific'))
             signal = int(row[1])
             door_open = row[2] == 'D1'
             motion_sensed = row[3] == 'M1'
-            print timestamp, signal, door_open, motion_sensed
+
+            #print timestamp, signal, door_open, motion_sensed
+
+            # build db object
             s = SensorData(timestamp=timestamp,
                            signal=signal,
                            door_open=door_open,
@@ -24,5 +30,5 @@ class Command(BaseCommand):
 
             s.save()
 
-            if i == 10:
-                break
+            if i % 100 == 0:
+                print 'Total Imported: %d' % i
