73cf5d3aa3b51b75f96c5d0052fe62d9ebd59a96
Patrick Rodriguez <patr84@gmail.com>
2013-12-22 19:19:40 -0800

    Added human readable string representation for SensorData

--- chicken/webapp/models.py
+++ chicken/webapp/models.py
@@ -9,3 +9,8 @@ class SensorData(models.Model):
 
     class Meta:
         unique_together = ('timestamp', 'signal', 'door_open', 'motion_sensed')
+
+    def __unicode__(self):
+        return '%s, Door Open: %s, Motion Sensed: %s' % (self.timestamp,
+                                                         self.door_open,
+                                                         self.motion_sensed)
